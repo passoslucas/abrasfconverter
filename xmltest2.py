734074369756e5708a6a18989902528916420dd0
passoslucas <48738729+passoslucas@users.noreply.github.com>
2020-02-27 10:22:36 -0300

    Organizando para impressao dos valores no layout Abrasf

--- xmltest2.py
+++ xmltest2.py
@@ -3,90 +3,167 @@ import xml.etree.ElementTree as ET
 mytree = ET.parse('nfs.xml')
 myroot = mytree.getroot()
 
-tomador_cnpj = (myroot[3][0].text)
-num_nota = (myroot[3][1].text)
-data_emissao = (myroot[3][2].text)
-sit_nota = (myroot[3][3].text)
-valor_bruto = (myroot[3][4].text)
-competencia = (myroot[3][5].text)
-prestador_cnpj = (myroot[3][8].text)
-prestador_nome = (myroot[3][10].text)
-aliquota = (myroot[3][12].text)
-iss_retido = (myroot[3][14].text)
-valor_liq = (myroot[3][15].text)
-valor_inss = ((myroot[3][17].text))
-desc_cond = (myroot[3][22].text)
-desc_incond = (myroot[3][23].text)
-discrim_serv = (myroot[3][24].text)
-cod_mun_serv = (myroot[3][25].text)
-cod_mun_serv = (myroot[3][28].text)
-tomador_nome = (myroot[3][31].text)
-valor_base_calc = (myroot[3][32].text)
-valor_iss_ret = (myroot[3][33].text)
-prestador_end_rua = (myroot[3][40].text)
+#cabecalho nota
+num_nota = int((myroot[3][1].text))
+cod_ver = (myroot[3][5].text)
+x = (myroot[3][2].text)
+data_emissao, hora_emissao , y = (x.split(' '))
+optante = int((myroot[3][79].text))
+competencia = (myroot[3][6].text)
+obs_nota = (myroot[3][25].text)
+
+#dados nota
+valor_bruto = float((myroot[3][4].text))
+deducoes = float((myroot[3][14].text))
+valor_inss = float((myroot[3][18].text))
+valor_ir = float((myroot[3][19].text))
+iss_retido = int((myroot[3][59].text))
+base_calc = float((myroot[3][32].text))
+aliquota = float((myroot[3][13].text))
+valor_liq = float((myroot[3][16].text))
+valor_iss_ret = float((myroot[3][33].text))
+desc_cond = float((myroot[3][23].text))
+desc_incond = float((myroot[3][24].text))
+
+#dados servico
+cod_serv = int((myroot[3][29].text))
+discrim_serv = (myroot[3][51].text)
+cod_mun_serv = int((myroot[3][26].text))
+
+#dados prestador
 prestador_cnpj = (myroot[3][36].text)
-prestador_end_cep = (myroot[3][39].text)
+prestador_nome = (myroot[3][11].text)
+prestador_end_rua = (myroot[3][40].text)
 prestador_end_num = (myroot[3][41].text)
 prestador_end_comp = (myroot[3][42].text)
 prestador_end_bairro = (myroot[3][43].text)
-prestador_end_mun = (myroot[3][44].text)
 prestador_cod_mun = (myroot[3][45].text)
-tomador_end_mun = (myroot[3][46].text)
-prestador_uf = (myroot[3][47].text)
+prestador_end_cep = (myroot[3][39].text)
 prestador_contato = (myroot[3][48].text)
+prestador_uf = (myroot[3][47].text)
+prestador_end_mun = (myroot[3][44].text)
+
+#dados tomador
+tomador_cnpj = (myroot[3][0].text)
+tomador_nome = (myroot[3][31].text)
 tomador_end_rua = (myroot[3][63].text)
 tomador_end_num = (myroot[3][64].text)
 tomador_end_bairro = (myroot[3][65].text)
 tomador_cod_mun = (myroot[3][66].text)
-tomador_uf = (myroot[3][67].text)
 tomador_end_cep = (myroot[3][68].text)
-valor_nota = (myroot[3][70].text)
-optante = (myroot[3][79].text)
+tomador_uf = (myroot[3][67].text)
+tomador_end_mun = (myroot[3][46].text)
+
+#dados pagamento
+valor_nota = float((myroot[3][70].text))
+
+#cancelamento
+cancelamento = 'false'
+sit_nota = (myroot[3][3].text)
+if sit_nota == 'A':
+    cancelamento = "false"
+else:
+    cancelamento = "true"
 
 
+print(valor_nota)
 
-print(tomador_cnpj, tomador_nome)
-
-
-
-# print(num_nota, ver_code, optante, outras_info, valor_servico, valor_inss, valor_ir, iss_retido, base_calculo, aliquota, valor_liq, valor_iss_ret, desc_cond, desc_incond)
-# print(cod_serv, discrim_serv, cod_mun_serv)
-# print(prestador_cnpj, prestador_im, prestador_nome)
-# print(prestador_end_rua, prestador_end_num, prestador_end_bairro, prestador_cod_mun, prestador_end_cep, prestador_contato, prestador_email)
-# print(tomador_cnpj, tomador_nome, tomador_end_rua, tomador_end_num, tomador_end_comp, tomador_end_bairro, tomador_cod_mun, tomador_uf)
-# print(valor_parcela)
-# print(cancelada)
-
-
-
-# for x in myroot.findall('ConsultarNfseResposta'):
-#     num_nota = x.find('Numero').text
-#     ver_code = x.find('CodigoVerificacao').text
-#     print('Nota: {} - Cod: {}'.format(num_nota, ver_code))
-
-
-
-# myroot[0].remove(myroot[3][0])
-# mytree.write('new4.xml')
-
-
-# myroot[0][0].attrib.pop('name')
-# mytree.write('new3.xml')
-
-
-# ET.SubElement(myroot[0], 'escritorio')
-
-# for x in myroot.iter('escritorio'):
-#     b = 'Passos Contabilidade'
-#     x.text = str(b)
-# mytree.write('new2.xml')
-
-
-# for x in myroot.iter('discriminacao'):
-#     a = str(x.text) + 'discriminacao has been added'
-#     x.text = str(a)
-#     x.set('updated', 'yes')
-
-# mytree.write('new.xml')
-
+print('<?xml version="1.0" encoding="ISO-8859-1" standalone="yes"?>')
+print('<ConsultarNfseResposta xmlns:ns2="http://www.w3.org/2000/09/xmldsig#">')
+print("    <ListaNfse>")
+print("        <CompNfse>")
+print("            <Nfse>")
+print("                <InfNfse>")
+print("                    <Numero>{}</Numero>".format(num_nota))
+print("                    <CodigoVerificacao>{}</CodigoVerificacao>".format(cod_ver))
+print("                    <DataEmissao>{}T{}.700-03:00</DataEmissao>".format(data_emissao, hora_emissao))
+print("                    <IdentificacaoRps/>")
+print("                    <NaturezaOperacao>2</NaturezaOperacao>")
+print("                    <OptanteSimplesNacional>{}</OptanteSimplesNacional>".format(optante))
+print("                    <Competencia>{}-01T00:00:00-03:00</Competencia>".format(competencia))
+print("                    <OutrasInformacoes>{}</OutrasInformacoes>".format(obs_nota))
+print("                    <Servico>")
+print("                        <Valores>")
+print("                            <ValorServicos>{}</ValorServicos>".format(valor_bruto))
+print("                            <ValorDeducoes>{}</ValorDeducoes>".format(deducoes))
+print("                            <ValorInss>{}</ValorInss>".format(valor_inss))
+print("                            <ValorIr>{}</ValorIr>".format(valor_ir))
+print("                            <IssRetido>{}</IssRetido>".format(iss_retido))
+print("                            <BaseCalculo>{}</BaseCalculo>".format(base_calc))
+print("                            <Aliquota>{}</Aliquota>".format(aliquota))
+print("                            <ValorLiquidoNfse>{}</ValorLiquidoNfse>".format(valor_liq))
+print("                            <ValorIssRetido>{}</ValorIssRetido>".format(valor_iss_ret))
+print("                            <DescontoCondicionado>{}</DescontoCondicionado>".format(desc_cond))
+print("                            <DescontoIncondicionado>{}</DescontoIncondicionado>".format(desc_incond))
+print("                        </Valores>")
+print("                        <ItemListaServico>{}</ItemListaServico>".format(cod_serv))
+print("                        <Discriminacao>{[[{}][ItemServico={}][Quantidade=1][ValorUnitario={}][ValorServico={}][ValorBaseCalculo={}][Aliquota={}][Deducoes={}][DescontoCondicionado={}][DescontoIncondicionado={}]]}</Discriminacao>".format(discrim_serv, cod_serv, valor_bruto, base_calc, aliquota, deducoes, desc_cond, desc_incond))
+print("                        <CodigoMunicipio>{}</CodigoMunicipio>".format(cod_mun_serv))
+print("                    </Servico>")
+print("                    <PrestadorServico>")
+print("                        <IdentificacaoPrestador>")
+print("                            <Cnpj>{}</Cnpj>".format(prestador_cnpj))
+print("                            <InscricaoMunicipal>430-0384/2762</InscricaoMunicipal>")
+print("                        </IdentificacaoPrestador>")
+print("                        <RazaoSocial>{}</RazaoSocial>".format(prestador_nome))
+print("                        <NomeFantasia>{}</NomeFantasia>".format(prestador_nome))
+print("                        <Endereco>")
+print("                            <Endereco>{}</Endereco>".format(prestador_end_rua))
+print("                            <Numero>{}</Numero>".format(prestador_end_num))
+print("                            <Bairro>{}</Bairro>".format(prestador_end_bairro))
+print("                            <CodigoMunicipio>{}</CodigoMunicipio>".format(prestador_cod_mun))
+print("                            <Cep>{}</Cep>".format(prestador_end_cep))
+print("                        </Endereco>")
+print("                        <Contato>")
+print("                            <Telefone>{}</Telefone>".format(prestador_contato))
+print("                            <Email></Email>")
+print("                        </Contato>")
+print("                    </PrestadorServico>")
+print("                    <TomadorServico>")
+print("                        <IdentificacaoTomador>")
+print("                            <CpfCnpj>")
+print("                                <Cnpj>{}</Cnpj>".format(tomador_cnpj))
+print("                            </CpfCnpj>")
+print("                        </IdentificacaoTomador>")
+print("                        <RazaoSocial>{}</RazaoSocial>".format(tomador_nome))
+print("                        <Endereco>")
+print("                            <Endereco>{}</Endereco>".format(tomador_end_rua))
+print("                            <Numero>{}</Numero>".format(tomador_end_num))
+print("                            <Complemento></Complemento>")
+print("                            <Bairro>{}</Bairro>".format(tomador_end_bairro))
+print("                            <CodigoMunicipio>{}</CodigoMunicipio>".format(tomador_cod_mun))
+print("                            <Uf>{}</Uf>".format(tomador_uf))
+print("                        </Endereco>")
+print("                        <Contato/>")
+print("                    </TomadorServico>")
+print("                    <IntermediarioServico/>")
+print("                    <ConstrucaoCivil/>")
+print("                    <CondicaoPagamento>")
+print("                        <Condicao>1</Condicao>")
+print("                        <QtdParcela>1</QtdParcela>")
+print("                        <Parcelas>")
+print("                            <Parcela>1</Parcela>")
+print("                            <Valor>{}</Valor>".format(valor_liq))
+print("                        </Parcelas>")
+print("                    </CondicaoPagamento>")
+print("                </InfNfse>")
+print("            </Nfse>")
+print("            <NfseCancelamento>")
+print("                <Confirmacao>")
+print("                    <Pedido>")
+print("                        <InfPedidoCancelamento/>")
+print("                        <ns2:Signature/>")
+print("                    </Pedido>")
+print("                    <InfConfirmacaoCancelamento>")
+print("                        <Sucesso>{}</Sucesso>".format(cancelamento))
+print("                    </InfConfirmacaoCancelamento>")
+print("                </Confirmacao>")
+print("            </NfseCancelamento>")
+print("            <NfseSubstituicao>")
+print("                <SubstituicaoNfse/>")
+print("            </NfseSubstituicao>")
+print("        </CompNfse>")
+print("    </ListaNfse>")
+print("    <ListaMensagemRetorno/>")
+print("</ConsultarNfseResposta>")
 
